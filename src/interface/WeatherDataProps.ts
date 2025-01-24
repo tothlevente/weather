b26bb30357a7e61cf0a04a24e87f5d794d60a732
Levente <168279599+tothlevente@users.noreply.github.com>
2025-01-24 11:30:11 +0000

    Develop WeatherDataProps.ts

--- src/interface/WeatherDataProps.ts
+++ src/interface/WeatherDataProps.ts
@@ -3,10 +3,19 @@ export default interface WeatherDataProps {
   main: {
     temp: number;
     feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
     humidity: number;
   };
   weather: {
     main: string;
     description: string;
+    icon: string;
   }[];
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
 }
