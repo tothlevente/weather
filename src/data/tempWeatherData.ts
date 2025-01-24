de4a691027615224f305bdac26de7dcffb3fe565
Levente <168279599+tothlevente@users.noreply.github.com>
2025-01-24 11:30:40 +0000

    Create temporary weather data

---
+++ src/data/tempWeatherData.ts
@@ -0,0 +1,27 @@
+import WeatherDataProps from "../interface/WeatherDataProps";
+
+const tempWeatherData: WeatherDataProps = {
+  weather: [
+    {
+      main: "Clouds",
+      description: "scattered clouds",
+      icon: "03d",
+    },
+  ],
+  main: {
+    temp: 10.53,
+    feels_like: 9.49,
+    temp_min: 9.78,
+    temp_max: 11.25,
+    pressure: 996,
+    humidity: 71,
+  },
+  wind: {
+    speed: 8.23,
+    deg: 230,
+    gust: 15.95,
+  },
+  name: "London",
+};
+
+export default tempWeatherData;
