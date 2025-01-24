3c80f02b34876697123329fe5ba9a74b0404ef3b
Levente <168279599+tothlevente@users.noreply.github.com>
2025-01-24 07:40:22 +0000

    Bugfix header components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,14 +10,14 @@ export default function Header({
   return (
     <div className="header">
       <div className="header-title">
-        <CloudSunIcon />
+        <CloudSunIcon className="header-icon" />
         <p>Weather</p>
       </div>
       <button
         className="header-toggle"
         onClick={toggleMode}
       >
-        {isDarkMode ? <MoonIcon className="w-5 h-5" /> : <SunIcon className="w-5 h-5" />}
+        {isDarkMode ? <MoonIcon /> : <SunIcon />}
       </button>
     </div>
   );
