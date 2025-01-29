a69dfbba7a6f5939a89c2c857c5f44a4f1baec6d
Levente <168279599+tothlevente@users.noreply.github.com>
2025-01-29 12:38:30 +0000

    Develop weather api document

--- src/api/WeatherApi.ts
+++ src/api/WeatherApi.ts
@@ -2,8 +2,22 @@ import Position from "@/interface/Position";
 
 import { Units } from "@/interface/Units";
 
+/**
+ * The API key for accessing the OpenWeatherMap API.
+ * This key is retrieved from the environment variables.
+ *
+ * @constant {string} apiKey - The OpenWeatherMap API key.
+ */
 const apiKey = import.meta.env.VITE_OPEN_WEATHER_MAP_API;
 
+/**
+ * Fetches weather data for a specified city.
+ *
+ * @param city - The name of the city to fetch weather data for.
+ * @param unit - The unit of measurement for the weather data (default is Metric).
+ * @returns A promise that resolves to the weather data for the specified city.
+ * @throws Will log an error message if the fetch operation fails.
+ */
 export async function GetWeatherDataByCity(
   city: string,
   unit: Units = Units.Metric
@@ -20,6 +34,14 @@ export async function GetWeatherDataByCity(
   }
 }
 
+/**
+ * Fetches weather data for a given geographical position.
+ *
+ * @param position - The geographical position (latitude and longitude).
+ * @param unit - The unit of measurement for temperature (default is Metric).
+ * @returns The weather data as a JSON object.
+ * @throws Will log an error message if the fetch operation fails.
+ */
 export async function GetWeatherDataByPosition(
   position: Position,
   unit: Units = Units.Metric
