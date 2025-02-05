4b5212553899861a77c40b39d5e0826f95399feb
Levente <168279599+tothlevente@users.noreply.github.com>
2025-02-05 12:01:27 +0000

    Develop header components

---
+++ src/components/LocateUser.tsx
@@ -0,0 +1,13 @@
+import { LocateFixedIcon } from "lucide-react";
+import { Button } from "./ui/button";
+
+export function LocateUser() {
+  return (
+    <Button
+      size="icon"
+      variant="outline"
+    >
+      <LocateFixedIcon />
+    </Button>
+  );
+}
--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,14 +1,18 @@
 import { CloudSunIcon } from "lucide-react";
 import { ModeToggle } from "../ModeToggle";
+import { LocateUser } from "../LocateUser";
 
 export default function Header() {
   return (
-    <div className="header">
-      <div className="header-title">
-        <CloudSunIcon className="header-icon" />
-        <p>Weather</p>
+    <div className="flex justify-between items-center m-5">
+      <div className="flex flex-nowrap flex-row justify-center">
+        <CloudSunIcon />
+        <p className="ml-1 font-bold">Weather</p>
       </div>
-      <ModeToggle />
+      <div className="grid grid-cols-2 gap-2">
+        <LocateUser />
+        <ModeToggle />
+      </div>
     </div>
   );
 }
